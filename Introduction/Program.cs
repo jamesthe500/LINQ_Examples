ddc648ec7c0f5aaa4e9027179579b6e9abe20167
Daniels <Daniels.James@mayo.edu>
2017-07-18 15:19:03 -0500

    Initial Commit. Makes a query of the file system without using LINQ. m1c2

---
+++ Introduction/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\windows";
+            ShowLargeFilesWithoutLinq(path);
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            // instance in teh System.io
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+
+            for (int i = 0; i<5; i++)
+            {
+                FileInfo file = files[i];
+                // after the commas is for formatting. -20 to left justify, 10 to right justify, :N0 means format as a number with commas with 0 decimal places
+                Console.WriteLine($"{file.Name, -20} : {file.Length, 10:N0}");
+            }
+        }
+    }
+
+    // this helper method was needed to help with the sorting.
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
+
+        }
+    }
+}
