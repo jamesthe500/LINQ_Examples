5e1ae3d94f9556eaeb76b66bc8a37f3962de0a77
Daniels <Daniels.James@mayo.edu>
2017-07-20 21:31:50 -0500

    solved with IEnumberable and no foreach. m2c2

--- Features/Program.cs
+++ Features/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+//using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,13 +10,15 @@ namespace Features
     {
         static void Main(string[] args)
         {
-            Employee[] developers = new Employee[]
+            //Employee[] developers = new Employee[]
+            IEnumerable<Employee> developers = new Employee[]
             {
                 new Employee {Id = 1, Name = "Scott" },
                 new Employee {Id = 2, Name = "Sarah" }
             };
 
-            List<Employee> sales = new List<Employee>()
+            //List<Employee> sales = new List<Employee>()
+            IEnumerable<Employee> sales = new List<Employee>()
             {
                 new Employee {Id = 3, Name = "Alexi" }
             };
@@ -24,9 +26,14 @@ namespace Features
             // A lot of LINQ operators off of IEnumberable<T> interface inheritance. 
             // it has flexibility to work with tyypes. can work with many differnt data sources
 
-            foreach (var person in sales)
+            // This is essentially a foreach doen the hard way. 
+            // done to demonstrate teh importance of IEnumerator to LINQ
+            // any kind of data sturcture or operation can be hidden behind IEnumerable
+
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            while (enumerator.MoveNext())
             {
-                Console.WriteLine(person.Name);
+                Console.WriteLine(enumerator.Current.Name);
             }
         }
     }
