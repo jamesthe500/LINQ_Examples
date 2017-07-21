00e01db2c55db2ab428c101f035f41d7c0be0fde
Daniels <Daniels.James@mayo.edu>
2017-07-21 18:10:03 -0500

    demonstrates and uses Fun<> type, applies to LINQ query. m2c5.

--- Features/Program.cs
+++ Features/Program.cs
@@ -10,6 +10,45 @@ namespace Features
     {
         static void Main(string[] args)
         {
+            // The Func type was created for working with delegates 
+            // which are types that allow you to create variables to point to methods. 
+            // the first parameter is the type the method takes in, second is what it returns
+            // of course it can point to a named method too
+            // this is an anonymous inline method using Lambda expression
+            Func<int, int> square = x => x * x;
+            Console.WriteLine(square(3));
+
+            // with more than two types defined, it's the last one that's the return, the others are inputs
+            // can explicitly define them to the right of the =, optional
+            Func<int, int, int> add = (int x, int y) => x + y;
+            Console.WriteLine(add(2,3));
+            Console.WriteLine(square(add(2,3)));
+
+            // can also use curly braces for more complex stuff
+            // needs to have a return statement
+            // though most Lambdas are short one-liners
+            Func<int, int, bool> evenSum = (x, y) =>
+            {
+                int temp = x + y;
+                int truthiness = temp % 2;
+                if (truthiness == 0)
+                {
+                    return true;
+                } else
+                {
+                    return false;
+                }
+            };
+
+            Console.WriteLine(evenSum(1,3));
+
+            // Action methods are another thing, though LINQ mostly uses Funcs
+            // they only return void, so all types laid out are for input.
+            Action<int> write = x => Console.WriteLine(x);
+            write(square(add(3, 4)));
+
+
+            Console.WriteLine( "****");
             
             IEnumerable<Employee> developers = new Employee[]
             {
@@ -24,36 +63,14 @@ namespace Features
                 new Employee {Id = 3, Name = "Alexi" }
             };
 
-            // original version that calls the named method below.
-            //foreach (var employee in developers.Where(NameStartsWithS))
-
-            // version 2. 
-            /*foreach (var employee in developers.Where(
-                delegate (Employee employee)
-                {
-                return employee.Name.StartsWith("S");
-                }))*/
-
-            // version 3 is the same as two, but using lamda expression
-            // here's how it's the same:
-            // complider doesn't need to be told it will be a delegate
-            // knows the type will have to be Employee, so not needed
-            // return, (), {}, ; also aren't needed
-            // left with the parameter name and expression 
-            // just add "Goes to" operator- =>
-            // shorten the parameter name for sleekness
-            foreach (var employee in developers.Where(
-            e => e.Name.StartsWith("S")
-            ))
+            
+            foreach (var employee in developers.Where(e => e.Name.Length == 5).OrderBy(e => e.Name))
             {
                 Console.WriteLine(employee.Name);
             }
         }
 
-        // this named method is a good way to filter for your needs. 
-        // it was created using intelisense, putting the name as a parameter in Where() above.
-        // In a big application, it would get cumbersome to do this over and over, so anonymous methods are better.
-        // so, taking the method definition below, paste it in above with some synax fixing up. add kw delegate
+       
         private static bool NameStartsWithS(Employee employee)
         {
             return employee.Name.StartsWith("S");
