2624845c2d5a03d76860c7e1b4287bac8a8a9a32
Daniels <Daniels.James@mayo.edu>
2017-07-20 21:48:49 -0500

    recreates the Count() method as an extension method. m2c3

---
+++ Features/MyLinq.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features
+{
+    public static class MyLinq
+    {
+        // adding the "this" to the parameter, it becomes an extension method
+        // making it availabel to any IEnumberable<T> within the same (or using) namespace
+        // with or without "this" it can be invoked with MyLinq.Count()
+        // 
+        public static int Count<T>(this IEnumerable<T> sequence)
+        {
+            int count = 0;
+            foreach (var item in sequence)
+            {
+                count += 1;
+            }
+            return count;
+        }
+    }
+}
--- Features/Program.cs
+++ Features/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+// commented it out so we could see how Linq works. There is already a Count() method
+// would work to not have our custom method as an extension method
 //using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,26 +12,21 @@ namespace Features
     {
         static void Main(string[] args)
         {
-            //Employee[] developers = new Employee[]
+            
             IEnumerable<Employee> developers = new Employee[]
             {
                 new Employee {Id = 1, Name = "Scott" },
                 new Employee {Id = 2, Name = "Sarah" }
             };
 
-            //List<Employee> sales = new List<Employee>()
+          
             IEnumerable<Employee> sales = new List<Employee>()
             {
                 new Employee {Id = 3, Name = "Alexi" }
             };
 
-            // A lot of LINQ operators off of IEnumberable<T> interface inheritance. 
-            // it has flexibility to work with tyypes. can work with many differnt data sources
-
-            // This is essentially a foreach doen the hard way. 
-            // done to demonstrate teh importance of IEnumerator to LINQ
-            // any kind of data sturcture or operation can be hidden behind IEnumerable
-
+            // can count arrays, lists, anything
+            Console.WriteLine(developers.Count());
             IEnumerator<Employee> enumerator = developers.GetEnumerator();
             while (enumerator.MoveNext())
             {
