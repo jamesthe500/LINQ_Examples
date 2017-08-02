d61018353521f002da6a4226fedffb6230a695f5
Daniels <Daniels.James@mayo.edu>
2017-08-02 17:59:24 -0500

    Ads filtering to the queries. m4c5

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -13,19 +13,51 @@ namespace Cars
         {
             var cars = ProcessFile("fuel.csv");
 
-            // .ThenBy() is the proper way to do a secondary sort. 
-            // you can tack on a .OrderBy, but that undoes teh previous sort
+            // where operator to filter the sort. Have to filter before sorting.
             /*
-            var query = cars.OrderByDescending(c => c.Combined)
-                            .ThenBy(c => c.Name);
-            */
-
-            // same as above but in query syntax
-            // "ascending" is an operator, but that's default, so not necessary
             var query =
                 from car in cars
-                orderby car.Combined ascending, car.Name ascending
+                where car.Manufacturer == "BMW" && car.Year == 2018
+                orderby car.Combined descending, car.Name ascending
                 select car;
+            */
+
+            // Same in extension method syntax
+            var query = cars.Where(c => c.Manufacturer == "BMW")
+                            .OrderByDescending(c => c.Combined)
+                            .ThenBy(c => c.Name);
+
+            // .First() has two overloads
+            // 1- it takes no parameters and just gives you first back
+            // 2- it takes a func and a bool, so it can replace the where 
+            /*
+            var topCar = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2018)
+                            .OrderByDescending(c => c.Combined)
+                            .ThenBy(c => c.Name)
+                            // doesn't give you anything to iterate over. cannot use deferred execution
+                            .First();
+            */
+
+            // Here is the way to do it with .First() replacing where
+            // must be careful about where you place that .First().
+            // if before sorting, you'll get the wrong results
+            /*
+            var topCar = cars
+                            .OrderByDescending(c => c.Combined)
+                            .ThenBy(c => c.Name)
+                            .First(c => c.Manufacturer == "BMW" && c.Year == 2018);
+            */
+
+            // there is also .Last()
+            // also .LastOrDefault() / .FirstOrDefault()
+            // These are handy if there is a chance that the dataset will be empty or your query could come up empty
+            // in these cases, it will return a defalt value, and error checking will have to happen down the line.
+            var topCar = cars
+                            .OrderByDescending(c => c.Combined)
+                            .ThenBy(c => c.Name)
+                            .LastOrDefault(c => c.Manufacturer == "Goof Balls" && c.Year == 2018);
+
+            Console.WriteLine(topCar.Name);
             
             foreach (var car in query.Take(10))
             {
