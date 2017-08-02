7fb29e1ba0068a4487ed301346ab3adec476d272
Daniels <Daniels.James@mayo.edu>
2017-08-02 18:14:05 -0500

    demonstrates ways of quantifying the dataset. m4c6

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -13,51 +13,28 @@ namespace Cars
         {
             var cars = ProcessFile("fuel.csv");
 
-            // where operator to filter the sort. Have to filter before sorting.
-            /*
+           
             var query =
                 from car in cars
                 where car.Manufacturer == "BMW" && car.Year == 2018
                 orderby car.Combined descending, car.Name ascending
                 select car;
-            */
 
-            // Same in extension method syntax
-            var query = cars.Where(c => c.Manufacturer == "BMW")
-                            .OrderByDescending(c => c.Combined)
-                            .ThenBy(c => c.Name);
+            // there are various quantifying operators
+            // they are all immediately executed.
+            // they try to be as lazy as possible, 
+            // so .Any() stops exectuing as soon as it finds a predicate match
 
-            // .First() has two overloads
-            // 1- it takes no parameters and just gives you first back
-            // 2- it takes a func and a bool, so it can replace the where 
-            /*
-            var topCar = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2018)
-                            .OrderByDescending(c => c.Combined)
-                            .ThenBy(c => c.Name)
-                            // doesn't give you anything to iterate over. cannot use deferred execution
-                            .First();
-            */
+            // .Any() can also be blank. Is there anything in this dataset?
+            //var result = cars.Any(c => c.Manufacturer == "Ford");
 
-            // Here is the way to do it with .First() replacing where
-            // must be careful about where you place that .First().
-            // if before sorting, you'll get the wrong results
-            /*
-            var topCar = cars
-                            .OrderByDescending(c => c.Combined)
-                            .ThenBy(c => c.Name)
-                            .First(c => c.Manufacturer == "BMW" && c.Year == 2018);
-            */
+            // Are all manufacturers ford?
+            //var result = cars.All(c => c.Manufacturer == "Ford");
 
-            // there is also .Last()
-            // also .LastOrDefault() / .FirstOrDefault()
-            // These are handy if there is a chance that the dataset will be empty or your query could come up empty
-            // in these cases, it will return a defalt value, and error checking will have to happen down the line.
-            var topCar = cars
-                            .OrderByDescending(c => c.Combined)
-                            .ThenBy(c => c.Name)
-                            .LastOrDefault(c => c.Manufacturer == "Goof Balls" && c.Year == 2018);
+            // Contains... Not really getting this one. :-/
+            var result = cars.Contains();
 
-            Console.WriteLine(topCar.Name);
+            Console.WriteLine(result );
             
             foreach (var car in query.Take(10))
             {
