45c8e72be9ab29783b6c1dfad2343d5d30b757f4
Daniels <Daniels.James@mayo.edu>
2017-08-18 17:32:09 -0500

    generates an XML from a csv using LINQ. Longhand form. m6c2

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,67 +12,30 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-            var cars = ProcessCars("fuel.csv");
-            var manufacturers = ProcessManufacturers("manufacturers.csv");
-            var carDetails = ProcessDetails("car_details2.csv");
+            var records = ProcessCars("fuel.csv");
 
-            var query =
-                from car in cars
-                group car by car.Manufacturer into carGroup
-                select new
-                {
-                    Name = carGroup.Key,
-                    Max = carGroup.Max(c => c.Combined),
-                    Min = carGroup.Min(c => c.Combined),
-                    Avg = carGroup.Average(c => c.Combined)
+            // creating an XML version of the CSV using System.Xml.Linq
+            var document = new XDocument();
+            var cars = new XElement("Cars");
 
-                } into result
-                orderby result.Max descending
-                select result;
-
-            // this extension methods way of doing it only loops through the dataset once. 
-            // whereas above it loops through 3 times, once per statisitc.
-            // obvs better. 
-            var query2 =
-               cars.GroupBy(c => c.Manufacturer)
-                    .Select(g =>
-                    {
-                        // here we pass in the accumilator class defined below.
-                        // an accumilator is needed as the first parameter to the Aggreagate() method
-                        // "results will be car statistics 
-                        // it's what happens when we Aggregate a grouping of cars"
-                        var results = g.Aggregate(new CarStatistics(),
-                                                    // second parameter takes teh accumulator and a car 
-                                                    // and does somethign to allow them to interact 
-                                                    // so stats can be tracked.
-                                                    // then returns the accumulator
-                                                    // "we pass in teh accumulator 
-                                                    // invoke the accumulate() once for each car"
-                                                    (acc, c) => acc.Accumulate(c),
-                                                    // thirdly, we generate the Compute() method below.
-                                                    // this computes the statistics we need.
-                                                    // "At the end we compute the final statistics
-                                                    acc => acc.Compute());
-                        return new
-                        {
-                            Name = g.Key,
-                            Avg = results.Avg,
-                            Max = results.Max,
-                            Min = results.Min
-                        };
-                    })
-                    .OrderByDescending(r => r.Max);
-
-            foreach (var result in query2)
+            foreach (var record in records)
             {
-                Console.WriteLine("");
-                Console.WriteLine($"{result.Name} :");
-                Console.WriteLine($"\t Max={result.Max}");
-                Console.WriteLine($"\t Min={result.Min}");
-                Console.WriteLine($"\t Avg={Math.Round(result.Avg, 1)}");
+                var car = new XElement("Car");
+                // there is implict conversion from string and int to the string of the XElement.
+                var name = new XElement("Name", record.Name);
+                var combined = new XElement("Combined", record.Combined);
 
+                // must explicitly add the data of these elements to the xML
+                car.Add(name);
+                car.Add(combined);
+                // This says to add teh car you've built to the document
+                cars.Add(car);
             }
-            
+            // add the entirety
+            document.Add(cars);
+            // a lot of options for how to save. This is just the filename.
+            document.Save("fuel.xml");
+                      
         }
 
         private static List<CarDetails> ProcessDetails(string path)
@@ -128,11 +92,8 @@ namespace Cars
         }
     }
 
-    // this custom class would normally be in a separate file, of course.
-    // Accumilator class
     public class CarStatistics
     {
-        // this is a constructor to set the initial values of these two variables
         public CarStatistics()
         {
             Max = Int32.MinValue;
@@ -144,27 +105,22 @@ namespace Cars
             Count++;
             Total += car.Combined;
 
-            // my solution.
             if(car.Combined > Max)
             {
                 Max = car.Combined;
             }
 
-            // this is the better way to do this that what I did with an if.
             Min = Math.Min(Min, car.Combined);
             
 
             return this;
         }
 
-        // this had to be changed to return CarStatistics and be a public method.
         public CarStatistics Compute()
         {
             Avg = Total / Count;
             return this;
         }
-
-        // the total field is needed to compute the Avg, of course.
         public int Max { get; set; }
         public int Min { get; set; }
         public int Total { get; set; }
